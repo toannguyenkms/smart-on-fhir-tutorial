dfbfba3d8cd9f73ae64cb302a0fd6fb55039fb52
toannguyenkms <84166056+toannguyenkms@users.noreply.github.com>
2021-05-17 14:53:58 +0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,6 +12,7 @@
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
+                    type: 'Observation',
                    
                   });
 
